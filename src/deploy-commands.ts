804824c82acbd7a00a0aad590529a6b61f1f011c
Definitely Not A Dolphin <derek.verduijn@outlook.com>
2025-06-19 23:29:18 +0200

    Quick push

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -1,7 +1,7 @@
-import { REST, Routes } from "discord.js";
-import config from "./../config.json" with { type: "json" };
 import fs from "node:fs";
 import path from "node:path";
+import { REST, Routes } from "discord.js";
+import config from "./../config.json" with { type: "json" };
 
 const commands = [];
 // Grab all the command folders from the commands directory you created earlier
